fd95672f10148f0590e93b2ec48a3387490524d2
Arturo Camargo <arturo.ripper@gmail.com>
2020-07-08 09:03:13 -0500

    Updates

---
+++ precache-manifest.00d282c5d394d82375f77f0ed9a9cd6a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce97a4b41a4513c2b7b07870bcdf36df",
+    "url": "/pcbs-calculator/index.html"
+  },
+  {
+    "revision": "4457280138f9bf740088",
+    "url": "/pcbs-calculator/static/css/2.c7fd0695.chunk.css"
+  },
+  {
+    "revision": "349803208a0d07aff7e4",
+    "url": "/pcbs-calculator/static/css/main.3079094b.chunk.css"
+  },
+  {
+    "revision": "4457280138f9bf740088",
+    "url": "/pcbs-calculator/static/js/2.1ecd701b.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/pcbs-calculator/static/js/2.1ecd701b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "349803208a0d07aff7e4",
+    "url": "/pcbs-calculator/static/js/main.a8805d83.chunk.js"
+  },
+  {
+    "revision": "b6b447b99eaef6ef6a09",
+    "url": "/pcbs-calculator/static/js/runtime-main.761ad3a5.js"
+  }
+]);
