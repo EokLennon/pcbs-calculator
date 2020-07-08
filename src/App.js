9cf3cc4fefea1ee979f78e0221806df14745d0be
Arturo Camargo <arturo.ripper@gmail.com>
2020-07-08 09:11:38 -0500

    Added gh-pages

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+// #region Main Imports
 import React from 'react';
 import { Layout } from 'antd';
 import {
@@ -6,45 +7,47 @@ import {
   Route,
   Redirect
 } from 'react-router-dom'
+// #endregion
 
-import SiderWithRouter from './components/SiderWithRouter';
+// #region Styles
+import './theme/style.scss';
+// #endregion
+
+// #region Screens
 import { ROUTES } from './constants';
-
 import ScoresScreen from './screens/ScoresScreen';
 import BuildMakerScreen from './screens/BuildMakerScreen';
 import BuildUpgraderScreen from './screens/BuildUpgraderScreen';
 import PartFixerScreen from './screens/PartFixerScreen';
 import HistoryScreen from './screens/HistoryScreen';
+// #endregion
 
-// Main Style
-import './theme/style.scss';
-// -------------
-
+// #region Components
+import SiderWithRouter from './components/SiderWithRouter';
 const { Content, Footer } = Layout;
+// #endregion 
 
-const App = () => {
-  return (
-    <Router>
-      <Layout className="main">
-        <SiderWithRouter />
-        <Layout className="site-layout">
-          <Content className="site-content">
-            <Switch>
-              <Route exact path={ROUTES.SCORES}   component={ScoresScreen} />
-              <Route exact path={ROUTES.MAKER}    component={BuildMakerScreen} />
-              <Route exact path={ROUTES.UPGRADER} component={BuildUpgraderScreen} />
-              <Route exact path={ROUTES.FIXER}    component={PartFixerScreen} />
-              <Route exact path={ROUTES.HISTORY}  component={HistoryScreen} />
-              <Redirect from="/" to={ROUTES.SCORES} />
-            </Switch>
-          </Content>
-          <Footer className="site-footer">
-            &copy;2020 Created by EokLennon
-          </Footer>
-        </Layout>
+const App = () => (
+  <Router basename="/pcbs-calculator">
+    <Layout className="main">
+      <SiderWithRouter />
+      <Layout className="site-layout">
+        <Content className="site-content">
+          <Switch>
+            <Route exact path={ROUTES.SCORES}   component={ScoresScreen} />
+            <Route exact path={ROUTES.MAKER}    component={BuildMakerScreen} />
+            <Route exact path={ROUTES.UPGRADER} component={BuildUpgraderScreen} />
+            <Route exact path={ROUTES.FIXER}    component={PartFixerScreen} />
+            <Route exact path={ROUTES.HISTORY}  component={HistoryScreen} />
+            <Redirect from="/" to={ROUTES.SCORES} />
+          </Switch>
+        </Content>
+        <Footer className="site-footer">
+          &copy;2020 Created by EokLennon
+        </Footer>
       </Layout>
-    </Router>
-  );
-}
+    </Layout>
+  </Router>
+);
 
 export default App;
